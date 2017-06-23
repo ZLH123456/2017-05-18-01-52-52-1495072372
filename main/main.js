70f2be065c0e37b158d37e8af250f82558337de1
ZLH123456 <1261407236@qq.com>
2017-06-24 00:44:13 +0800

    sasa
    asasasa

--- main/main.js
+++ main/main.js
@@ -1,18 +1,39 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+    console.log('o) 最小值 = '+sequence.minimum()+
+'\no) 最大值 = '+sequence.maxmu()+
+'\no) 元素数量 = '+sequence.len+
+'\no) 平均值 = '+sequence.average());
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+      this.len = input.length;
+      this.input = input;
   }
 
   minimum() {
-    // Write your code here
+    var min=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]<min)
+        min=this.input[i];
+    }
+    return min;
   }
 
-  // Write your code here
+  maxmu(){
+    var max=this.input[0];
+    for(var i=1;i<this.len;i++) {
+        if(this.input[i]>max)
+        max=this.input[i];
+    }
+    return max;
+  }
+  average(){
+	  var av=0;
+	  for(var i=0;i<this.len;i++) {
+        av=av+this.input[i];
+    }
+    return (av/this.len).toFixed(2);
+  }
 }
